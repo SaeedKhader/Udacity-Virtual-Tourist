2cce3159bd7f45903cf2710fd97431c3e6a4d045
Saeed Khader <saeed.khader@outlook.com>
2019-12-07 14:54:13 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Saeed Khader on 07/12/2019.
+//  Copyright © 2019 Saeed Khader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
